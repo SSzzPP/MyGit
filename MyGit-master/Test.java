3035a81fb2a4c17462985036cbec8a4993a97b06
SSzzPP <zxsunzhangping@126.com>
2019-02-27 00:08:49 +0800

    clone

---
+++ MyGit-master/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("爱生活，爱Java");
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("来了老弟");
+	}
+}
