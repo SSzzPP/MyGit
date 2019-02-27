6980127e0776e1906ff160eea43f491647ee4b3d
SSzzPP <48024268+SSzzPP@users.noreply.github.com>
2019-02-27 22:50:34 +0800

    Delete Test.java

--- MyGit-master/Test.java
+++
@@ -1,9 +0,0 @@
-public class Test {
-	public static void main(String[] args) {
-		System.out.println("爱生活，爱Java");
-	}
-	
-	public static void main(String[] args) {
-		System.out.println("来了老弟");
-	}
-}
